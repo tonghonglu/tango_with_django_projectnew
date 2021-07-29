3a2a64c10617ee66bca324c87ed5003c85b7378f
tongs <2528915t@student.gla.ac.uk>
2021-07-29 22:11:17 +0800

    ch5 2nd

--- populate_rango.py
+++ populate_rango.py
@@ -57,7 +57,8 @@ def add_page(cat, title, url, views=0):
 
 def add_cat(name, views=0, likes=0):
     c = Category.objects.get_or_create(name=name)[0]
-
+    c.views = views
+    c.likes = likes
     c.save()
     return c
 
